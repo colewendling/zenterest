4f0d3ef453e38ea144ea61151a8eb2cf85e76e4e
colewendling <colewendling@utexas.edu>
2021-02-12 19:39:31 -0600

    organize style sheet / match navbar styling

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 
 
 
@@ -11,20 +11,16 @@ class Greeting extends React.Component {
 
 
         <div className="nav-bar">
-            
-                <div className="header-icon">
-                <img className="favicon-medium" src={window.logoURL} alt="favicon" />
-                </div>
-               
-                <div className="header-buttons">
+            <div className="nav-bar-child-left">
+               {/* <Link to='/'><img src={window.znRedURL} className="zn" /></Link> */}
+               <Link to='/'><img src={window.znRedURL} className="zn" /></Link>
+               <h3>Zenterest</h3>
+            </div>
+            <nav className="nav-bar-child-right">
                     <button className="red-button" onClick={() => openModal('login')}>Login</button>
                     <button className="grey-button" onClick={() => openModal('signup')}>Signup</button>
-                </div>
+            </nav>
         </div>
-
-
-
-
     );
    }
  };
