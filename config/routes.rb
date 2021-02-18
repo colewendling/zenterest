da1ccd51dd5658a03ca7ffcbbfb2a6f019ddc0a1
colewendling <colewendling@utexas.edu>
2021-02-18 12:12:13 -0600

    add routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  root "static_pages#root"
+
   namespace :api, defaults: {format: :json} do
-    resource :users, only: [:create, :index, :show]
+    resources :users, only: [:create, :index, :show, :update, :destroy]
+    resources :boards, only: [:create, :edit, :update, :destroy, :index, :show]
+    resources :pins, only: [:create, :edit, :update, :destroy, :show]
     resource :session, only: [:create, :destroy]
   end
-  root "static_pages#root"
+  
 end
