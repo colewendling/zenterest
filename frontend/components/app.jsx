225a413d9370f9fe46cad193ea83f946aadc5578
colewendling <colewendling@utexas.edu>
2021-02-18 13:03:54 -0600

    app.jsx route logic

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import NavSplashContainer from './navbars/nav_splash_container';
 import Modal from './modal/modal';
-import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+import { Switch} from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
-import NavBarContainer from "./navbars/nav_internal_container";
+
+import NavBar from "./navbars/nav_internal_container";
+import NavSplash from './navbars/nav_splash_container';
+
+// import Profile from '../components/profiles/profile_container';
+// import ProfilePins from './profile/profile_container';
+
+// import PinIndex from './pin/pin_index_container';
+// import PinShow  from './pin/pin_show_container';
+
+// import BoardIndex from './board/board_index_container';
+// import BoardShow from './board/board_show_container'
+
 
 
 const App = () => (
 <div> 
-  <div>
+
+  <header>
     <Modal />
-    <header>
-    </header>
-  </div>
-  <AuthRoute exact path='/' 
-  component={NavSplashContainer} />
-  <ProtectedRoute exact path='/home' component={NavBarContainer} />
+  </header>
+
+  <AuthRoute exact path='/' component={NavSplash} /> 
+  <ProtectedRoute exact path='/home' component={NavBar} /> 
+
   <Switch>
+    {/* <ProtectedRoute exact path='/home' component={PinIndex} />
+    <ProtectedRoute exact path='/users/:userId' component={Profile} />
+    <ProtectedRoute exact path='/users/:userId/boards' component={BoardIndex} />
+    <ProtectedRoute exact path='/users/:userId/boards/:boardId' component={BoardShow} />
+    <ProtectedRoute exact path='/users/:userId/pins' component={ProfilePins} />
+    <ProtectedRoute exact path='/pins/:pinId' component={PinShow} /> */}
   </Switch>
+
 </div>
 );
            
