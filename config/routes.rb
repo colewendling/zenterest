6f76e68c990fd667f4c3fc5e6be4aadfa2ed7b28
colewendling <colewendling@utexas.edu>
2021-02-18 12:19:12 -0600

    fix routes so the user profile has users/:id/boards for profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
+    
+    resources :users, only: [:create, :index, :show, :update, :destroy] do
+      resources :boards, only: [:index]
+      resources :pins, only: [:index]
+    end
+
     resources :users, only: [:create, :index, :show, :update, :destroy]
-    resources :boards, only: [:create, :edit, :update, :destroy, :index, :show]
+    resources :boards, only: [:create, :edit, :update, :destroy, :show]
     resources :pins, only: [:create, :edit, :update, :destroy, :show]
     resource :session, only: [:create, :destroy]
   end
-  
+
 end
