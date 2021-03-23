4eedbabe2631b9cad8124f972c2b7a1835c52a18
colewendling <colewendling@utexas.edu>
2021-03-23 17:45:40 -0500

    add pin show link back to board - .scss styling on pin show

--- frontend/components/pin/pin_show.jsx
+++ frontend/components/pin/pin_show.jsx
@@ -7,31 +7,34 @@ class PinShow extends React.Component {
 
   render() {
     const pin = this.props.pins[this.props.id]
-    // debugger
+    const board = this.props.boards[pin.board_id]
     return (
       <div className='pin-show'>
-        <button className='modal-close-x' onClick={() => {this.props.closeModal()}}>X</button>
+        <button className='modal-close-x' onClick={() => { this.props.closeModal() }}>X</button>
+        <i className='fa fa-angle-left' id='board-back' onClick={() => { this.props.openModal(['boardShow', pin.board_id])}}></i>
+        <button className="pin-delete-button" onClick={() => {
+          this.props.deletePin(pin.id)
+          this.props.closeModal()
+          window.location.reload();
+        }}>Delete
+        </button>
         <div className='pin-show-left'>
           <img src={pin.imageUrl} alt={pin.title} />
         </div>
         <div className='pin-show-right'>
-        <div className='pin-show-info-container'>
-          <h1 className='pin-show-title'>{pin.title}</h1>
-          <hr />
-          <p className='pin-show-description'>{pin.description}</p>
-          <hr />
-            <i onClick={() => this.props.openModal(['boardShow', pin.board_id])} class="fas fa-arrow-right"></i>
-        </div>
-          <div className='pin-delete-button-container'>
-            <button className="pin-delete-button" onClick={() => {
-                this.props.deletePin(pin.id)
-                this.props.closeModal()
-                window.location.reload();
-              }}>Delete
-            </button>
+          <div className='pin-show-info-container'>
+            <div>
+              <h1 className='pin-show-title'>{pin.title}</h1>
+              <hr />
+            </div>
+            <div>
+              <p className='pin-show-description'>{pin.description}</p>
+              <hr />
+            </div>
+            <i className='pin-show-board-link' onClick={() => this.props.openModal(['boardShow', pin.board_id])}>Board | {board.title}</i>    
           </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
--- frontend/components/pin/pin_show_container.js
+++ frontend/components/pin/pin_show_container.js
@@ -3,11 +3,13 @@ import { withRouter } from 'react-router-dom';
 import { fetchPin, deletePin, fetchAllPins } from '../../actions/pin_actions';
 import { fetchUser } from '../../actions/user_actions';
 import { closeModal, openModal } from '../../actions/modal_actions';
+import { fetchAllBoards } from '../../actions/board_actions';
 import PinShow from './pin_show';
 
 const mapStateToProps = (state) => {
   return ({
     pins: state.entities.pins,
+    boards: state.entities.boards,
     currentUser: state.entities.users[state.session.currentUser],
     boards: state.entities.boards,
   })
@@ -15,6 +17,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => ({
   fetchAllPins: () => dispatch(fetchAllPins()),
+  fetchAllBoards: () => dispatch(fetchAllBoards()),
   deletePin: pinId => dispatch(deletePin(pinId)),
   fetchPin: pinId => dispatch(fetchPin(pinId)),
   fetchUser: userId => dispatch(fetchUser(userId)),
