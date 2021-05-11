7a71083628cd41eb1df066c2c9d399ce66dd7525
colewendling <colewendling@utexas.edu>
2021-05-11 10:41:50 -0500

    make boards reducer match pins reducer

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,6 +1,4 @@
 import * as UsersApiUtil from '../util/user_api_util';
-
-
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
 export const RECEIVE_USER_ERRORS = 'RECEIVE_USER_ERRORS';
--- frontend/reducers/boards_reducer.js
+++ frontend/reducers/boards_reducer.js
@@ -1,19 +1,14 @@
-import { RECEIVE_ALL_USER_BOARDS, RECEIVE_ALL_BOARDS, RECEIVE_BOARD, REMOVE_BOARD } from '../actions/board_actions'
+import { RECEIVE_ALL_BOARDS, RECEIVE_ALL_BOARDS, RECEIVE_BOARD, REMOVE_BOARD } from '../actions/board_actions'
 
 const boardsReducer = (state = {}, action) => {
   Object.freeze(state)
-  let newState = Object.assign({}, state);
-  
   switch (action.type) {
     case RECEIVE_ALL_BOARDS:
       return action.boards;
-    case RECEIVE_ALL_USER_BOARDS:
-      action.userBoards.boards.forEach((board) => 
-      {newState[board.id] = board });
-      return newState;
     case RECEIVE_BOARD:
       return Object.assign({}, { [action.board.id]: action.board });
     case REMOVE_BOARD:
+      let newState = Object.assign({}, state)
       delete newState[action.boardId];
       return newState;
     default:
