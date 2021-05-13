a9c68f2f1f5fae735f4ce3a1d1db84dc9080e7cc
colewendling <colewendling@utexas.edu>
2021-05-13 11:00:04 -0500

    finish clean up/initial refactor to add board component functionality

--- frontend/zenterest.jsx
+++ frontend/zenterest.jsx
@@ -5,31 +5,27 @@ import Root from './components/root'
 import { login, logout } from './actions/session_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  let store;
 
-
-
-    const root = document.getElementById("root");
-    let store;
-    if (window.currentUser) {
-      const preloadedState = {
-        entities: {
-          users: { [window.currentUser.id]: window.currentUser } 
-        },
-        session: { currentUser: window.currentUser }
-      };
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser } 
+      },
+      session: { currentUser: window.currentUser }
+    };
     store = configureStore(preloadedState)
     delete window.currentUser;
-    } else {
-      store = configureStore();
-    }
-
-    
-    window.login = login;
-    window.logout = logout;
-    window.state = store.getState;
-    window.dispatch = store.dispatch; 
-
- 
-    ReactDOM.render(<Root store={store} />, root);
-
+    } 
+  else {
+    store = configureStore();
+  }
+  
+  window.login = login;
+  window.logout = logout;
+  window.state = store.getState;
+  window.dispatch = store.dispatch; 
+  
+  ReactDOM.render(<Root store={store} />, root);
 });
