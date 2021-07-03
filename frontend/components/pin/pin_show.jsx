b5694296b650c08d7c917307945fead4c277a27b
colewendling <colewendling@utexas.edu>
2021-07-02 19:51:42 -0500

    add pin delete button

--- frontend/components/pin/pin_show.jsx
+++ frontend/components/pin/pin_show.jsx
@@ -5,6 +5,8 @@ class PinShow extends React.Component {
 
   constructor(props) {
     super(props);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   render() {
@@ -17,11 +19,22 @@ class PinShow extends React.Component {
           <img src={pin.imageUrl} alt={pin.title} />
         </div>
         <div className='pin-show-info-container'>
+          <div className='pin-delete-button-container'>
+          <button className="pin-delete-button" onClick={() => {
+            this.props.deletePin(pin.id)
+            this.props.closeModal()
+            }}><i className="far fa-times-circle"></i></button>
+
+
+            
+          </div>
           <h1 className='pin-show-title'>{pin.title}</h1>
           <hr />
           <p className='pin-show-description'>{pin.description}</p>
-          <button></button>
+       
+
         </div>
+
       </div>
     
     );
