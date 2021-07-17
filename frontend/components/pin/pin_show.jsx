39a00c53822c45198ec70a5c1dc3cc8e5d1ebcc0
colewendling <colewendling@utexas.edu>
2021-07-17 18:25:00 -0500

    pin show css

--- frontend/components/pin/pin_show.jsx
+++ frontend/components/pin/pin_show.jsx
@@ -10,18 +10,20 @@ class PinShow extends React.Component {
   render() {
     const pin = this.props.pins[this.props.id]
     return (
-      // <div className="create-modal-container">
-      <div className='pin-show-container'>
-      {/* <h1 className='pin-show-title'>Pin Show</h1> */}
-        <div className='pin-show-image-container'>
-          <img className='pin-show-image' src={pin.imageUrl} alt={pin.title} />
+    
+      <div className='pin-show'>
+    
+        <div className='pin-show-image'>
+          <img src={pin.imageUrl} alt={pin.title} />
         </div>
         <div className='pin-show-info-container'>
-          <h2 className='pin-show-title'>{pin.title}</h2>
+          <h1 className='pin-show-title'>{pin.title}</h1>
+          <hr />
           <p className='pin-show-description'>{pin.description}</p>
+          <button></button>
         </div>
       </div>
-    // </div>
+    
     );
   }
 }
